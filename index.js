b667a9191258c82931676b3b71bd05c9364278c5
danielgottbehuet <daniel.gottbehuet@t-online.de>
2021-06-22 09:51:53 +0200

    .

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-'use strict';
-const path = require('path');
-const BinWrapper = require('bin-wrapper');
-
-module.exports = new BinWrapper()
-	.src('https://github.com/eugeneware/ffmpeg-static/releases/download/b4.4/darwin-x64', 'darwin')
-	.dest(path.join(__dirname, './vendor'))
-	.use('darwin-x64');
+"use strict";
+module.exports = require("./lib").path();
